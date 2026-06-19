8cdbda87e6eb47520fca0388283c9b6cb3eaa337
Muhammad Ahsan Munir <ahsanmuneer81@gmail.com>
2026-06-19 11:47:49 +0400

    preparation

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,36 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import ReactQueryExample from "./screens/ReactQueryExample";
-import ZustandExample from "./screens/ZustandExample";
-
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 export type RootStackParamList = {
-  Testing: undefined;
-  ReactQueryExample: undefined;
-  ZustandExample: undefined;
+  HomeScreen: undefined;
+  LoginScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-// QueryClient holds the cache — create once at app level
-const queryClient = new QueryClient();
-
 export default function App() {
   return (
-    // QueryClientProvider makes React Query available to every screen
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: "#1a1a2e" },
-            headerTintColor: "#fff",
-            headerTitleStyle: { fontWeight: "600" },
-          }}
-        >
-          {/* <Stack.Screen
-            name="Testing"
-            component={TestingScreen}
-            options={{ title: "Testing" }}
-          /> */}
-          <Stack.Screen
-            name="ReactQueryExample"
-            component={ReactQueryExample}
-            options={{ title: "React Query" }}
-          />
-          <Stack.Screen
-            name="ZustandExample"
-            component={ZustandExample}
-            options={{ title: "Zustand" }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </QueryClientProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="LoginScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#1a1a2e" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "600" },
+        }}
+      >
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ title: "Home" }}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ title: "Login" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
