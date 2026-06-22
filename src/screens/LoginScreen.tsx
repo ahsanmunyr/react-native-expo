b1c7c27d9c8b56a0075d12b65e595d88f2e28f45
Muhammad Ahsan Munir <ahsanmuneer81@gmail.com>
2026-06-22 12:28:54 +0400

    Interview repo added

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,41 +1,7 @@
-import { memo, useCallback, useMemo, useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import createStyles from "../style/LoginScreen.style";
+import { memo } from "react";
+import { View } from "react-native";
 const LoginScreen = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const style = useMemo(() => createStyles(), []);
-
-  const handleLogin = useCallback(() => {
-    // Implement your login logic here
-    console.log("Logging in with:", { username, password });
-  }, [username, password]);
-
-  return (
-    <View style={style.container}>
-      <Text>LoginScreen</Text>
-      <View style={{ marginTop: 20, padding: 10, gap: 12 }}>
-        <TextInput
-          style={style.input}
-          placeholder="username"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <TextInput
-          style={style.input}
-          secureTextEntry
-          placeholder="password"
-          value={password}
-          onChangeText={setPassword}
-        />
-      </View>
-      <View style={{}}>
-        <TouchableOpacity onPress={handleLogin} style={style.button}>
-          <Text style={style.buttonText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+  return <View style={style.container}></View>;
 };
 
 export default memo(LoginScreen);
